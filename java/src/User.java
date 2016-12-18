5886789c1cbc6bbea732fc7e8a298f8af54e6f64
tico2303 <tico82003@gmail.com>
2016-12-17 16:52:59 -0800

    updated User class

--- java/src/User.java
+++ java/src/User.java
@@ -1,18 +1,41 @@
 
-
 public class User
 {
-    private String name;
-    private String passwd;
-    private String type;
+    private String _login;
+    private String _passwd;
+    private String _type;
     
-
-    public void setName(String name)
+///////////////////////////////////////////
+//   Setters
+///////////////////////////////////////////
+    public void setLogin(String login)
     {
-
+       this._login = login; 
     }
-    public void setPassword(String name)
-    public void setType(String name)
+    public void setPassword(String passwd)
+    {
+       this._passwd = passwd; 
+    }
+    public void setType(String type)
+    {
+        this._type = type;
+    }
 
-
-}
+///////////////////////////////////////////
+//   Getters
+///////////////////////////////////////////
+    
+    public String getLogin()
+    {
+        return this._login
+    }
+    public String getPassword()
+    {
+        return this._passwd
+    }
+    public String getType()
+    {
+        return this._type
+    }
+    
+}//end User
